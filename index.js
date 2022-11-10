7ebed2f59110b7e1409636f3bb0d79b9e019226d
DylanSantiago <2dylansantiago@gmail.com>
2022-11-10 09:57:45 -0500

    view and add employee functions created

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const connection = mysql.createConnection({
     host: "localhost",
     port: 3001,
     user:"root",
-    password: process.env.password,
+    password: process.env.DB_PASSWORD,
     database: "organization_DB"
 });
 
@@ -52,22 +52,23 @@ function viewDatabase() {
                 case "Add Role":
                     addRole();
                     break;
-            }
-        })
+                case "Quit":
+                    connection.end();
+                    break;
+            };
+        });
 
 };
 
 function viewEmployees() {
     connection.query(
         `SELECT
-        employee.id,
-        CONCAT (employee.first_name, " ", employee.last_name) 
-        AS name, role.title, role.salary,
+        employee.id, employee.first_name, employee.last_name, 
+        role.title, role.salary, department.name AS department,
         CONCAT (manager.first_name, " ", manager.last_name) 
-        AS manager, department.name AS department
-        FROM employee
-        JOIN role on employee.role_id = role.id
-        JOIN department on role.department_id = department.id
+        AS manager FROM employee
+        LEFT JOIN role on employee.role_id = role.id
+        LEFT JOIN department on role.department_id = department.id
         LEFT JOIN employee manager ON employee.manager_id = manager.id`,
         function (err, result) {
             if (err) {
@@ -96,3 +97,53 @@ function viewRoles() {
         viewDatabase();
     });
 }
+
+function addEmployee() {
+    inquirer
+        .prompt([
+            {
+                type:"input",
+                name:"first_name?",
+                message: "What is the new employee's first name?",
+            },
+            {
+                type:"input",
+                name:"last_name?",
+                message: "What is the new employee's last name?",
+            },
+            {
+                type:"input",
+                name:"role",
+                message: "What position is this employee being hired for?",
+            },
+            {
+                type:"input",
+                name:"manager",
+                message: "Who does the new employee report to? List by manager ID",
+            },
+        ])
+        .then(function ({data}) {
+            connection.query("INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?,?,?,?)"),
+            [data.first_name, data.last_name, data.role, data.manager],
+            function (err, res) {
+                if (err) throw err;
+            }
+        });
+        connection.query(
+        `SELECT
+        employee.id, employee.first_name, employee.last_name, 
+        role.title, role.salary, department.name AS department,
+        CONCAT (manager.first_name, " ", manager.last_name) 
+        AS manager FROM employee
+        LEFT JOIN role on employee.role_id = role.id
+        LEFT JOIN department on role.department_id = department.id
+        LEFT JOIN employee manager ON employee.manager_id = manager.id`,
+        function (err, result) {
+            if (err) throw err;
+            console.table(result);
+            viewDatabase();
+        }
+        )
+
+}
+
